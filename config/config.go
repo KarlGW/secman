ab7a91b986e56d348f289426e0a080d8e8b93397
Karl Wallenius <karl.wallenius@gmail.com>
2023-08-01 11:19:02 +0200

    feat: basic configuration

--- config/config.go
+++ config/config.go
@@ -1,1 +1,102 @@
 package config
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/KarlGW/secman/internal/fs"
+	"gopkg.in/yaml.v3"
+)
+
+const (
+	dir        = ".secman"
+	configFile = "config.yaml"
+)
+
+// configuration for the application.
+type configuration struct {
+	path string
+	key  *[32]byte
+}
+
+// Option sets options to the configuration.
+type Option func(c *configuration)
+
+// Configure creates and returns a configuration.
+func Configure(options ...Option) (cfg configuration, err error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return
+	}
+
+	cfg.path = filepath.Join(home, dir)
+	for _, option := range options {
+		option(&cfg)
+	}
+
+	configFile, err := fs.OpenWithCreateIfNotExist(filepath.Join(cfg.path, configFile))
+	if err != nil {
+		return
+	}
+	defer func() {
+		if e := configFile.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	b, err := io.ReadAll(configFile)
+	if err != nil {
+		return
+	}
+
+	if err = cfg.FromYAML(b); err != nil {
+		return
+	}
+
+	return cfg, err
+}
+
+// YAML returns the YAML encoding of the configuration.
+func (c configuration) YAML() []byte {
+	var buf bytes.Buffer
+	encoder := yaml.NewEncoder(&buf)
+	encoder.SetIndent(2)
+	encoder.Encode(c)
+
+	return buf.Bytes()
+}
+
+// FromYAML sets configuration from yaml.
+func (c *configuration) FromYAML(b []byte) error {
+	return yaml.Unmarshal(b, c)
+}
+
+// Save the configuration to local storage.
+func (c *configuration) Save() (err error) {
+	file, err := fs.OpenWithCreateIfNotExist(filepath.Join(c.path, configFile))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := file.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	_, err = file.Write(c.YAML())
+	return err
+}
+
+// Key returns the key configured the application.
+func (c configuration) Key() *[32]byte {
+	return c.key
+}
+
+// WithKey sets the key on the configuration.
+func WithKey(key *[32]byte) Option {
+	return func(c *configuration) {
+		c.key = key
+	}
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,8 @@ module github.com/KarlGW/secman
 
 go 1.20
 
-require github.com/google/go-cmp v0.5.9
-
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/go-cmp v0.5.9
+	github.com/google/uuid v1.3.0
+	gopkg.in/yaml.v3 v3.0.1
+)
