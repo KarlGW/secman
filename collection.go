18a737bd51570f466537fbdb9a9ccc21d62f162a
Karl Wallenius <karl.wallenius@gmail.com>
2023-08-02 09:37:46 +0200

    feat: make secrets private, add functions go retreive, add and remove secrets from a collection

--- collection.go
+++ collection.go
@@ -8,17 +8,153 @@ import (
 
 // Collection represents a collection of secrets.
 type Collection struct {
-	Secrets      []Secret
-	LastModified time.Time
+	secrets       []Secret
+	lastModified  time.Time
+	secretsByID   map[string]int
+	secretsByName map[string]int
+}
+
+// Secrets returns all the secrets in the collection.
+func (c Collection) Secrets() []Secret {
+	return c.secrets
+}
+
+// Secret returns a secret by ID.
+func (c Collection) Secret(id string) Secret {
+	return c.SecretByID(id)
+}
+
+// SecretByID returns a secret by the provided ID.
+func (c Collection) SecretByID(id string) Secret {
+	i, ok := c.secretsByID[id]
+	if !ok {
+		return Secret{}
+	}
+	return c.secrets[i]
+}
+
+// SecretByName returns a secret by the provided name.
+func (c Collection) SecretByName(name string) Secret {
+	i, ok := c.secretsByName[name]
+	if !ok {
+		return Secret{}
+	}
+	return c.secrets[i]
+}
+
+// Add a secret to the collection. Returns true if secret was added,
+// false if not (secret already exists).
+func (c *Collection) Add(secret Secret) bool {
+	if c.SecretByID(secret.ID).Valid() || c.SecretByName(secret.Name).Valid() {
+		return false
+	}
+
+	// If maps are not set, initialize them.
+	if c.secretsByID == nil {
+		c.secretsByID = make(map[string]int)
+	}
+	if c.secretsByName == nil {
+		c.secretsByName = make(map[string]int)
+	}
+
+	c.secrets = append(c.secrets, secret)
+	index := len(c.secrets) - 1
+
+	c.secretsByID[secret.ID] = index
+	c.secretsByName[secret.Name] = index
+	c.lastModified = now()
+	return true
+}
+
+// Remove a secret by the provided ID.
+func (c *Collection) Remove(id string) bool {
+	return c.RemoveByID(id)
+}
+
+// RemoveByID removes a secret by the provided ID.
+func (c *Collection) RemoveByID(id string) bool {
+	i, ok := c.secretsByID[id]
+	if !ok {
+		return false
+	}
+
+	c.remove(i)
+	c.lastModified = now()
+
+	return true
+}
+
+// RemoveByID removes a secret by the provided name.
+func (c *Collection) RemoveByName(name string) bool {
+	i, ok := c.secretsByName[name]
+	if !ok {
+		return false
+	}
+
+	c.remove(i)
+	c.lastModified = now()
+
+	return true
+}
+
+// remove the secret by index and update the index maps.
+func (c *Collection) remove(i int) {
+	c.secrets = append(c.secrets[:i], c.secrets[i+1:]...)
+	for k, v := range c.secretsByID {
+		if v == i {
+			delete(c.secretsByID, k)
+		}
+		if v > i {
+			c.secretsByID[k]--
+		}
+	}
+	for k, v := range c.secretsByName {
+		if v == i {
+			delete(c.secretsByName, k)
+		}
+		if v > i {
+			c.secretsByName[k]--
+		}
+	}
+}
+
+// LastModified returns when the collection was last modified.
+func (c Collection) LastModified() time.Time {
+	return c.lastModified
+}
+
+// encodedCollection is used for encoding a collection.
+type encodedCollection struct {
+	Secrets       []Secret
+	LastModified  time.Time
+	SecretsByID   map[string]int
+	SecretsByName map[string]int
 }
 
 // Encode a collection to a gob.
 func (c Collection) Encode() []byte {
-	encoded, _ := gob.Encode(c)
+	collection := encodedCollection{
+		Secrets:       c.secrets,
+		LastModified:  c.lastModified,
+		SecretsByID:   c.secretsByID,
+		SecretsByName: c.secretsByName,
+	}
+
+	encoded, _ := gob.Encode(collection)
 	return encoded
 }
 
 // Decode a collection from a gob.
 func (c *Collection) Decode(data []byte) error {
-	return gob.Decode(data, c)
+	var collection encodedCollection
+	if err := gob.Decode(data, &collection); err != nil {
+		return err
+	}
+
+	c.secrets = collection.Secrets
+	c.lastModified = collection.LastModified
+	c.secretsByID = collection.SecretsByID
+	c.secretsByName = collection.SecretsByName
+
+	return nil
 }
--- collection_test.go
+++ collection_test.go
@@ -2,6 +2,7 @@ package secman
 
 import (
 	"testing"
+	"time"
 
 	"github.com/google/go-cmp/cmp"
 )
@@ -16,20 +17,20 @@ func TestCollection_Encode_Decode(t *testing.T) {
 		{
 			name: "Encode and Decode a collection",
 			input: Collection{
-				Secrets: []Secret{
+				secrets: []Secret{
 					{
 						Name: "secret",
 					},
 				},
-				LastModified: _testTime1,
+				lastModified: _testTime1,
 			},
 			want: Collection{
-				Secrets: []Secret{
+				secrets: []Secret{
 					{
 						Name: "secret",
 					},
 				},
-				LastModified: _testTime1,
+				lastModified: _testTime1,
 			},
 			wantErr: nil,
 		},
@@ -40,7 +41,7 @@ func TestCollection_Encode_Decode(t *testing.T) {
 			var got Collection
 			gotErr := got.Decode(test.input.Encode())
 
-			if diff := cmp.Diff(test.want, got); diff != "" {
+			if diff := cmp.Diff(test.want, got, cmp.AllowUnexported(Collection{})); diff != "" {
 				t.Errorf("Encode()/Decode() = unexpected result (-want +got)\n%s\n", diff)
 			}
 
@@ -48,6 +49,923 @@ func TestCollection_Encode_Decode(t *testing.T) {
 				t.Errorf("Encode()/Decode() = unexpected error (-want +got)\n%s\n", diff)
 			}
 
+		})
+	}
+}
+
+func TestCollection_Add(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input struct {
+			collection Collection
+			secret     Secret
+		}
+		want       Collection
+		wantResult bool
+	}{
+		{
+			name: "Add secret to empty collection",
+			input: struct {
+				collection Collection
+				secret     Secret
+			}{
+				collection: Collection{},
+				secret: Secret{
+					ID:   "1",
+					Name: "secret",
+				},
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret",
+					},
+				},
+				lastModified: _testLastModified,
+				secretsByID: map[string]int{
+					"1": 0,
+				},
+				secretsByName: map[string]int{
+					"secret": 0,
+				},
+			},
+			wantResult: true,
+		},
+		{
+			name: "Add secret to collection with existing secrets",
+			input: struct {
+				collection Collection
+				secret     Secret
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+					},
+				},
+				secret: Secret{
+					ID:   "3",
+					Name: "secret-3",
+				},
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+					{
+						ID:   "3",
+						Name: "secret-3",
+					},
+				},
+				lastModified: _testLastModified,
+				secretsByID: map[string]int{
+					"1": 0,
+					"2": 1,
+					"3": 2,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+					"secret-2": 1,
+					"secret-3": 2,
+				},
+			},
+			wantResult: true,
+		},
+		{
+			name: "Add secret to collection (secret with ID already exist)",
+			input: struct {
+				collection Collection
+				secret     Secret
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+					},
+					lastModified: time.Date(2023, 8, 2, 12, 30, 0, 0, time.Local),
+				},
+				secret: Secret{
+					ID:   "1",
+					Name: "secret-new",
+				},
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+				},
+				lastModified: time.Date(2023, 8, 2, 12, 30, 0, 0, time.Local),
+				secretsByID: map[string]int{
+					"1": 0,
+					"2": 1,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+					"secret-2": 1,
+				},
+			},
+			wantResult: false,
+		},
+		{
+			name: "Add secret to collection (secret with name already exist)",
+			input: struct {
+				collection Collection
+				secret     Secret
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+					},
+					lastModified: time.Date(2023, 8, 2, 12, 30, 0, 0, time.Local),
+				},
+				secret: Secret{
+					ID:   "3",
+					Name: "secret-1",
+				},
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+				},
+				lastModified: time.Date(2023, 8, 2, 12, 30, 0, 0, time.Local),
+				secretsByID: map[string]int{
+					"1": 0,
+					"2": 1,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+					"secret-2": 1,
+				},
+			},
+			wantResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			now = func() time.Time {
+				return _testLastModified
+			}
+			gotResult := test.input.collection.Add(test.input.secret)
+			got := test.input.collection
+
+			if diff := cmp.Diff(test.want, got, cmp.AllowUnexported(Collection{}, Secret{})); diff != "" {
+				t.Errorf("Add() = unexpected result (-want +got)\n%s\n", diff)
+			}
+
+			if test.wantResult != gotResult {
+				t.Errorf("Add() = unexpected result, want: %t, got: %t\n", test.wantResult, gotResult)
+			}
+
+		})
+	}
+}
+
+func TestCollection_RemoveByID(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input struct {
+			collection Collection
+			id         string
+		}
+		want       Collection
+		wantResult bool
+	}{
+		{
+			name: "Remove a secret, one secret in collection",
+			input: struct {
+				collection Collection
+				id         string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+					},
+				},
+				id: "1",
+			},
+			want: Collection{
+				secrets:       []Secret{},
+				lastModified:  _testLastModified,
+				secretsByID:   map[string]int{},
+				secretsByName: map[string]int{},
+			},
+			wantResult: true,
+		},
+		{
+			name: "Remove a secret from a collection of multiple secrets (remove from middle)",
+			input: struct {
+				collection Collection
+				id         string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+						{
+							ID:   "3",
+							Name: "secret-3",
+						},
+						{
+							ID:   "4",
+							Name: "secret-4",
+						},
+						{
+							ID:   "5",
+							Name: "secret-5",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+						"3": 2,
+						"4": 3,
+						"5": 4,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+						"secret-3": 2,
+						"secret-4": 3,
+						"secret-5": 4,
+					},
+				},
+				id: "3",
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+					{
+						ID:   "4",
+						Name: "secret-4",
+					},
+					{
+						ID:   "5",
+						Name: "secret-5",
+					},
+				},
+				lastModified: _testLastModified,
+				secretsByID: map[string]int{
+					"1": 0,
+					"2": 1,
+					"4": 2,
+					"5": 3,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+					"secret-2": 1,
+					"secret-4": 2,
+					"secret-5": 3,
+				},
+			},
+			wantResult: true,
+		},
+		{
+			name: "Remove a secret from a collection of multiple secrets (remove from beginning)",
+			input: struct {
+				collection Collection
+				id         string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+						{
+							ID:   "3",
+							Name: "secret-3",
+						},
+						{
+							ID:   "4",
+							Name: "secret-4",
+						},
+						{
+							ID:   "5",
+							Name: "secret-5",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+						"3": 2,
+						"4": 3,
+						"5": 4,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+						"secret-3": 2,
+						"secret-4": 3,
+						"secret-5": 4,
+					},
+				},
+				id: "1",
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+					{
+						ID:   "3",
+						Name: "secret-3",
+					},
+					{
+						ID:   "4",
+						Name: "secret-4",
+					},
+					{
+						ID:   "5",
+						Name: "secret-5",
+					},
+				},
+				lastModified: _testLastModified,
+				secretsByID: map[string]int{
+					"2": 0,
+					"3": 1,
+					"4": 2,
+					"5": 3,
+				},
+				secretsByName: map[string]int{
+					"secret-2": 0,
+					"secret-3": 1,
+					"secret-4": 2,
+					"secret-5": 3,
+				},
+			},
+			wantResult: true,
+		},
+		{
+			name: "Remove a secret from a collection of multiple secrets (remove from end)",
+			input: struct {
+				collection Collection
+				id         string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+						{
+							ID:   "3",
+							Name: "secret-3",
+						},
+						{
+							ID:   "4",
+							Name: "secret-4",
+						},
+						{
+							ID:   "5",
+							Name: "secret-5",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+						"3": 2,
+						"4": 3,
+						"5": 4,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+						"secret-3": 2,
+						"secret-4": 3,
+						"secret-5": 4,
+					},
+				},
+				id: "5",
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+					{
+						ID:   "3",
+						Name: "secret-3",
+					},
+					{
+						ID:   "4",
+						Name: "secret-4",
+					},
+				},
+				lastModified: _testLastModified,
+				secretsByID: map[string]int{
+					"1": 0,
+					"2": 1,
+					"3": 2,
+					"4": 3,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+					"secret-2": 1,
+					"secret-3": 2,
+					"secret-4": 3,
+				},
+			},
+			wantResult: true,
+		},
+		{
+			name: "secret with id does not exist",
+			input: struct {
+				collection Collection
+				id         string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+					},
+				},
+				id: "2",
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+				},
+				secretsByID: map[string]int{
+					"1": 0,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+				},
+			},
+			wantResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			now = func() time.Time {
+				return _testLastModified
+			}
+
+			gotResult := test.input.collection.RemoveByID(test.input.id)
+			got := test.input.collection
+
+			if diff := cmp.Diff(test.want, got, cmp.AllowUnexported(Collection{})); diff != "" {
+				t.Errorf("RemoveByID() = unexpected result (-want +got)\n%s\n", diff)
+			}
+
+			if test.wantResult != gotResult {
+				t.Errorf("RemoveByID() = unexpected result, want: %t, got: %t\n", test.wantResult, gotResult)
+			}
+		})
+	}
+}
+
+func TestCollection_RemoveByName(t *testing.T) {
+	var tests = []struct {
+		name  string
+		input struct {
+			collection Collection
+			name       string
+		}
+		want       Collection
+		wantResult bool
+	}{
+		{
+			name: "Remove a secret, one secret in collection",
+			input: struct {
+				collection Collection
+				name       string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+					},
+				},
+				name: "secret-1",
+			},
+			want: Collection{
+				secrets:       []Secret{},
+				lastModified:  _testLastModified,
+				secretsByID:   map[string]int{},
+				secretsByName: map[string]int{},
+			},
+			wantResult: true,
+		},
+		{
+			name: "Remove a secret from a collection of multiple secrets (remove from middle)",
+			input: struct {
+				collection Collection
+				name       string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+						{
+							ID:   "3",
+							Name: "secret-3",
+						},
+						{
+							ID:   "4",
+							Name: "secret-4",
+						},
+						{
+							ID:   "5",
+							Name: "secret-5",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+						"3": 2,
+						"4": 3,
+						"5": 4,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+						"secret-3": 2,
+						"secret-4": 3,
+						"secret-5": 4,
+					},
+				},
+				name: "secret-3",
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+					{
+						ID:   "4",
+						Name: "secret-4",
+					},
+					{
+						ID:   "5",
+						Name: "secret-5",
+					},
+				},
+				lastModified: _testLastModified,
+				secretsByID: map[string]int{
+					"1": 0,
+					"2": 1,
+					"4": 2,
+					"5": 3,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+					"secret-2": 1,
+					"secret-4": 2,
+					"secret-5": 3,
+				},
+			},
+			wantResult: true,
+		},
+		{
+			name: "Remove a secret from a collection of multiple secrets (remove from beginning)",
+			input: struct {
+				collection Collection
+				name       string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+						{
+							ID:   "3",
+							Name: "secret-3",
+						},
+						{
+							ID:   "4",
+							Name: "secret-4",
+						},
+						{
+							ID:   "5",
+							Name: "secret-5",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+						"3": 2,
+						"4": 3,
+						"5": 4,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+						"secret-3": 2,
+						"secret-4": 3,
+						"secret-5": 4,
+					},
+				},
+				name: "secret-1",
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+					{
+						ID:   "3",
+						Name: "secret-3",
+					},
+					{
+						ID:   "4",
+						Name: "secret-4",
+					},
+					{
+						ID:   "5",
+						Name: "secret-5",
+					},
+				},
+				lastModified: _testLastModified,
+				secretsByID: map[string]int{
+					"2": 0,
+					"3": 1,
+					"4": 2,
+					"5": 3,
+				},
+				secretsByName: map[string]int{
+					"secret-2": 0,
+					"secret-3": 1,
+					"secret-4": 2,
+					"secret-5": 3,
+				},
+			},
+			wantResult: true,
+		},
+		{
+			name: "Remove a secret from a collection of multiple secrets (remove from end)",
+			input: struct {
+				collection Collection
+				name       string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+						{
+							ID:   "2",
+							Name: "secret-2",
+						},
+						{
+							ID:   "3",
+							Name: "secret-3",
+						},
+						{
+							ID:   "4",
+							Name: "secret-4",
+						},
+						{
+							ID:   "5",
+							Name: "secret-5",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+						"2": 1,
+						"3": 2,
+						"4": 3,
+						"5": 4,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+						"secret-2": 1,
+						"secret-3": 2,
+						"secret-4": 3,
+						"secret-5": 4,
+					},
+				},
+				name: "secret-5",
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+					{
+						ID:   "2",
+						Name: "secret-2",
+					},
+					{
+						ID:   "3",
+						Name: "secret-3",
+					},
+					{
+						ID:   "4",
+						Name: "secret-4",
+					},
+				},
+				lastModified: _testLastModified,
+				secretsByID: map[string]int{
+					"1": 0,
+					"2": 1,
+					"3": 2,
+					"4": 3,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+					"secret-2": 1,
+					"secret-3": 2,
+					"secret-4": 3,
+				},
+			},
+			wantResult: true,
+		},
+		{
+			name: "secret with name does not exist",
+			input: struct {
+				collection Collection
+				name       string
+			}{
+				collection: Collection{
+					secrets: []Secret{
+						{
+							ID:   "1",
+							Name: "secret-1",
+						},
+					},
+					secretsByID: map[string]int{
+						"1": 0,
+					},
+					secretsByName: map[string]int{
+						"secret-1": 0,
+					},
+				},
+				name: "secret-2",
+			},
+			want: Collection{
+				secrets: []Secret{
+					{
+						ID:   "1",
+						Name: "secret-1",
+					},
+				},
+				secretsByID: map[string]int{
+					"1": 0,
+				},
+				secretsByName: map[string]int{
+					"secret-1": 0,
+				},
+			},
+			wantResult: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			now = func() time.Time {
+				return _testLastModified
+			}
+
+			gotResult := test.input.collection.RemoveByName(test.input.name)
+			got := test.input.collection
+
+			if diff := cmp.Diff(test.want, got, cmp.AllowUnexported(Collection{})); diff != "" {
+				t.Errorf("RemoveByName() = unexpected result (-want +got)\n%s\n", diff)
+			}
+
+			if test.wantResult != gotResult {
+				t.Errorf("RemoveByName() = unexpected result, want: %t, got: %t\n", test.wantResult, gotResult)
+			}
 		})
 	}
 }
