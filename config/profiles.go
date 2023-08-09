2d442c250d39083d1be8b96a232b4563854959a2
Karl Wallenius <karl.wallenius@gmail.com>
2023-08-10 01:02:31 +0200

    refactor: update handling of profiles and config, add keychain handling

---
+++ config/profiles.go
@@ -0,0 +1,84 @@
+package config
+
+import (
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+
+	"github.com/KarlGW/secman/internal/fs"
+	"github.com/google/uuid"
+	"gopkg.in/yaml.v3"
+)
+
+// profile contains information of a user profile.
+type profile struct {
+	ID          string `yaml:"id"`
+	Name        string `yaml:"name"`
+	DisplayName string `yaml:"displayName,omitempty"`
+	Description string `yaml:"description,omitempty"`
+}
+
+// profile contains profiles.
+type profiles struct {
+	p    map[string]profile
+	path string
+}
+
+// YAML returns the YAML encoding of the profiles.
+func (p profiles) YAML() []byte {
+	var buf bytes.Buffer
+	encoder := yaml.NewEncoder(&buf)
+	encoder.SetIndent(2)
+	encoder.Encode(p.p)
+
+	return buf.Bytes()
+}
+
+// FromYAML sets profiles from yaml.
+func (p *profiles) FromYAML(b []byte) error {
+	return yaml.Unmarshal(b, p.p)
+}
+
+// Load profiles from a yaml file.
+func (p *profiles) Load(file *os.File) error {
+	b, err := io.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	return p.FromYAML(b)
+}
+
+// Save the profiles to file.
+func (p profiles) Save() (err error) {
+	file, err := fs.OpenWithCreateIfNotExist(filepath.Join(p.path, profilesFile))
+	if err != nil {
+		return err
+	}
+	defer func() {
+		if e := file.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	_, err = file.Write(p.YAML())
+	return err
+}
+
+// GetByName gets a profile by name.
+func (p profiles) GetByName(name string) profile {
+	for k, v := range p.p {
+		if v.Name == name {
+			return p.p[k]
+		}
+	}
+	return profile{}
+}
+
+// newProfile creates and returns a new profile.
+func newProfile(name string) profile {
+	return profile{
+		ID:   uuid.New().String(),
+		Name: name,
+	}
+}
--- go.mod
+++ go.mod
@@ -1,9 +1,17 @@
 module github.com/KarlGW/secman
 
-go 1.20
+go 1.21.0
 
 require (
 	github.com/google/go-cmp v0.5.9
 	github.com/google/uuid v1.3.0
+	github.com/zalando/go-keyring v0.2.3
 	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/alessio/shellescape v1.4.1 // indirect
+	github.com/danieljoos/wincred v1.2.0 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
 )
