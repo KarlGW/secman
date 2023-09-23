f9c0682027b4d03475f3cb0eb0901e1f52018e17
Karl Wallenius <karl.wallenius@gmail.com>
2023-09-23 11:27:29 +0200

    feat: add passwordPrompt function, update parameter name to ctx

--- command/command.go
+++ command/command.go
@@ -2,11 +2,14 @@ package command
 
 import (
 	"errors"
+	"syscall"
 
 	"github.com/KarlGW/secman/config"
+	"github.com/KarlGW/secman/output"
 	"github.com/KarlGW/secman/secret"
 	"github.com/KarlGW/secman/storage"
 	"github.com/urfave/cli/v2"
+	"golang.org/x/term"
 )
 
 var (
@@ -15,18 +18,18 @@ var (
 )
 
 // configure creates a configuration and sets it to the provided *cli.Context.
-func configure(c *cli.Context) error {
+func configure(ctx *cli.Context) error {
 	cfg, err := config.Configure()
 	if err != nil {
 		return err
 	}
-	c.App.Metadata["config"] = cfg
+	ctx.App.Metadata["config"] = cfg
 	return nil
 }
 
 // configuration returns the configuration from the provided *cli.Context.
-func configuration(c *cli.Context) (config.Configuration, error) {
-	cfg, ok := c.App.Metadata["config"].(config.Configuration)
+func configuration(ctx *cli.Context) (config.Configuration, error) {
+	cfg, ok := ctx.App.Metadata["config"].(config.Configuration)
 	if !ok {
 		return config.Configuration{}, ErrUnableToRetrieveConfig
 	}
@@ -35,8 +38,8 @@ func configuration(c *cli.Context) (config.Configuration, error) {
 
 // initHandler performs the necessary steps to setup a handler and
 // set it to the provided *cli.Context.
-func initHandler(c *cli.Context) error {
-	config, err := configuration(c)
+func initHandler(ctx *cli.Context) error {
+	config, err := configuration(ctx)
 	if err != nil {
 		return err
 	}
@@ -49,23 +52,34 @@ func initHandler(c *cli.Context) error {
 
 	handler, err := secret.NewHandler(
 		config.ProfileID,
-		config.Key(),
 		config.StorageKey(),
+		config.Key(),
 		storage.NewFileSystem(config.StoragePath()),
 		secret.WithLoadCollection(),
 	)
 	if err != nil {
 		return err
 	}
-	c.App.Metadata["handler"] = handler
+	ctx.App.Metadata["handler"] = handler
 	return nil
 }
 
 // handler retrieves the handler from the provided *cli.Context.
-func handler(c *cli.Context) (*secret.Handler, error) {
-	handler, ok := c.App.Metadata["handler"].(*secret.Handler)
+func handler(ctx *cli.Context) (*secret.Handler, error) {
+	handler, ok := ctx.App.Metadata["handler"].(*secret.Handler)
 	if !ok {
 		return nil, ErrUnableToRetieveHandler
 	}
 	return handler, nil
+}
+
+// passwordPrompt prompts for entering a password.
+func passwordPrompt() ([]byte, error) {
+	output.Print("Enter password: ")
+	p, err := term.ReadPassword(int(syscall.Stdin))
+	if err != nil {
+		return nil, err
+	}
+	output.PrintEmptyln()
+	return p, nil
 }
