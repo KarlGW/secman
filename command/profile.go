4c2ad7ac18a149079452d6f9b5a0f5a7493f32c7
Karl Wallenius <karl.wallenius@gmail.com>
2023-09-23 11:28:34 +0200

    feat: update all secrets with new password/key, cleanup

--- command/profile.go
+++ command/profile.go
@@ -1,12 +1,8 @@
 package command
 
 import (
-	"syscall"
-
 	"github.com/KarlGW/secman/internal/security"
-	"github.com/KarlGW/secman/output"
 	"github.com/urfave/cli/v2"
-	"golang.org/x/term"
 )
 
 // Profile is the command containing subcommands for handling
@@ -14,37 +10,77 @@ import (
 func Profile() *cli.Command {
 	return &cli.Command{
 		Name: "profile",
+		Subcommands: []*cli.Command{
+			ProfileUpdate(),
+		},
+		Before: func(ctx *cli.Context) error {
+			return configure(ctx)
+		},
+	}
+}
+
+// ProfileGet is a subcommand for getting profiles.
+func ProfileGet() *cli.Command {
+	return &cli.Command{}
+}
+
+// ProfileUpdate is a subcommand for updating profiles.
+func ProfileUpdate() *cli.Command {
+	return &cli.Command{
+		Name:    "update",
+		Aliases: []string{"set"},
 		Flags: []cli.Flag{
 			&cli.BoolFlag{
 				Name:    "password",
 				Aliases: []string{"p"},
 			},
 		},
-		Before: func(ctx *cli.Context) error {
-			return configure(ctx)
-		},
 		Action: func(ctx *cli.Context) error {
-			cfg, err := configuration(ctx)
-			if err != nil {
-				return err
-			}
-
 			if ctx.IsSet("password") {
-				output.Print("Enter password: ")
-				password, err := term.ReadPassword(int(syscall.Stdin))
-				if err != nil {
+				if err := setPassword(ctx); err != nil {
 					return err
 				}
-				key, err := security.NewKeyFromPassword(password)
-				if err != nil {
-					return err
-				}
-				if err := cfg.SetKey(key); err != nil {
-					return err
-				}
-				output.PrintEmptyln()
 			}
 			return nil
 		},
 	}
+}
+
+// setPassword takes the provided password and creates a new key from it
+// and sets it to the provided configuration and updates all
+// the secrets contained in the handler.
+func setPassword(ctx *cli.Context) error {
+	password, err := passwordPrompt()
+	if err != nil {
+		return err
+	}
+
+	key, err := security.NewKeyFromPassword(password)
+	if err != nil {
+		return err
+	}
+
+	cfg, err := configuration(ctx)
+	if err != nil {
+		return err
+	}
+
+	if cfg.Key().Valid() {
+		if err := initHandler(ctx); err != nil {
+			return err
+		}
+		handler, err := handler(ctx)
+		if err != nil {
+			return err
+		}
+		if err := handler.UpdateKey(key); err != nil {
+			return err
+		}
+	}
+
+	if err := cfg.SetKey(key); err != nil {
+		return err
+	}
+
+	return nil
 }
