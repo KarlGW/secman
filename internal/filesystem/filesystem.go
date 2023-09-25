a2de836d2a242dc09fb28947359f7da851178d98
Karl Wallenius <karl.wallenius@gmail.com>
2023-09-25 10:33:17 +0200

    docs: fix doc comments for filesystem.OpenFile

--- internal/filesystem/filesystem.go
+++ internal/filesystem/filesystem.go
@@ -6,8 +6,8 @@ import (
 	"path/filepath"
 )
 
-// OpenFile wraps around the standard library fs.OpenFile with addition
-// of creating the path to the file if it doesn't exist.
+// OpenFile wraps around the standard library os.OpenFile with addition
+// of creating the path to the file if it doesn't exist with os.MkdirAll.
 func OpenFile(name string, flag int, perm fs.FileMode) (*os.File, error) {
 	if err := os.MkdirAll(filepath.Dir(name), 0700); err != nil {
 		return nil, err
