df742b72c939e6f6b64e383a01354ac465f81fc0
Karl <karl.wallenius@gmail.com>
2023-09-25 09:39:51 +0200

    feat: add completion command with completion scripts for bash, zsh and powershell (#5)

---
+++ command/completion.go
@@ -0,0 +1,40 @@
+package command
+
+import (
+	"github.com/KarlGW/secman/completion"
+	"github.com/KarlGW/secman/output"
+	"github.com/urfave/cli/v2"
+)
+
+func Completion() *cli.Command {
+	return &cli.Command{
+		Name:  "completion",
+		Usage: "Generate completion script",
+		Subcommands: []*cli.Command{
+			{
+				Name:  "bash",
+				Usage: "Generate bash completion script",
+				Action: func(ctx *cli.Context) error {
+					output.Println(completion.Bash())
+					return nil
+				},
+			},
+			{
+				Name:  "zsh",
+				Usage: "Generate zsh completion script",
+				Action: func(ctx *cli.Context) error {
+					output.Println(completion.Zsh())
+					return nil
+				},
+			},
+			{
+				Name:  "powershell",
+				Usage: "Generate PowerShell completion script",
+				Action: func(ctx *cli.Context) error {
+					output.Println(completion.PowerShell())
+					return nil
+				},
+			},
+		},
+	}
+}
