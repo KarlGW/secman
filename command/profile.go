d7fd584dbac0740f5a4a6032950506a56d98f88c
Karl Wallenius <karl.wallenius@gmail.com>
2023-10-23 23:45:26 +0200

    feat: foundation for import and export profile commands

--- command/profile.go
+++ command/profile.go
@@ -1,6 +1,8 @@
 package command
 
 import (
+	"errors"
+
 	"github.com/KarlGW/secman/internal/security"
 	"github.com/urfave/cli/v2"
 )
@@ -14,6 +16,8 @@ func Profile() *cli.Command {
 		Category: "Subcommands",
 		Subcommands: []*cli.Command{
 			ProfileUpdate(),
+			ProfileExport(),
+			ProfileImport(),
 		},
 		Before: func(ctx *cli.Context) error {
 			return configure(ctx)
@@ -46,6 +50,44 @@ func ProfileUpdate() *cli.Command {
 				}
 			}
 			return nil
+		},
+	}
+}
+
+// ProfileExport is a subcommand for exporting profiles.
+func ProfileExport() *cli.Command {
+	return &cli.Command{
+		Name:  "export",
+		Usage: "Export profile",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "file",
+				Aliases:  []string{"f"},
+				Usage:    "File to export to.",
+				Required: true,
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			return exportProfile(ctx)
+		},
+	}
+}
+
+// ProfileImport is a subcommand for importing profiles.
+func ProfileImport() *cli.Command {
+	return &cli.Command{
+		Name:  "import",
+		Usage: "Import profile",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "file",
+				Aliases:  []string{"f"},
+				Usage:    "file to import from",
+				Required: true,
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			return importProfile(ctx)
 		},
 	}
 }
@@ -86,5 +128,46 @@ func setPassword(ctx *cli.Context) error {
 		return err
 	}
 
+	return nil
+}
+
+// exportProfile exports profile after promptong for configured password,
+// and then prompting for password on target file.
+func exportProfile(ctx *cli.Context) error {
+	cfg, err := configuration(ctx)
+	if err != nil {
+		return err
+	}
+
+	var password []byte
+	tries := 0
+	for {
+		password, err = passwordPrompt()
+		if err != nil {
+			return err
+		}
+		ok := security.ComparePasswordAndKey(password, cfg.Key())
+		if ok {
+			break
+		}
+		tries++
+		if tries == 3 && !ok {
+			return errors.New("wrong password")
+		}
+	}
+
+	password, err = passwordPrompt("Set password for output file: ")
+	if err != nil {
+		return err
+	}
+	key, err := security.NewSHA256FromPassword(password)
+	if err != nil {
+		return err
+	}
+	return cfg.Export(ctx.String("file"), key)
+}
+
+// importProfile after a valid password has been entered.
+func importProfile(ctx *cli.Context) error {
 	return nil
 }
